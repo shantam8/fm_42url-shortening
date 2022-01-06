13656d8c0c2ae142116cb22eac2ba547d37b5440
shantam8 <dscharmen@aol.com>
2022-01-06 13:34:54 +0100

    effects and animations done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
+let btnToggleMobileMenu = document.querySelector("#btnToggleMobileMenu");
+let menuBlock = document.querySelector("#menuBlock");
+let body = document.querySelector("body");
+let backdrop = document.querySelector("#backdrop");
 
-function init(){
-    console.log("hi");
+function toggleMobileMenu() {
+  if (menuBlock.classList.contains("open")) {
+    setTimeout(() => {
+      menuBlock.style.display = "none";
+    }, 500);
+    menuBlock.classList.toggle("open");
+  } else {
+      menuBlock.style.display = "block";
+      setTimeout(() => {
+        menuBlock.classList.toggle("open");
+    }, 100);
+  }
+
+  btnToggleMobileMenu.classList.toggle("menuOpen");
+  body.classList.toggle("backdropActive");
+  backdrop.classList.toggle("active");
+}
+
+function init() {
+  console.log("hi");
+  btnToggleMobileMenu.addEventListener("click", toggleMobileMenu);
 }
 
 window.onload = init();
